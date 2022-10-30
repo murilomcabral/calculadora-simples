3e2751645c7ac5623a9e416fcf54c3f60205504a
murilomcabral <95389598+murilomcabral@users.noreply.github.com>
2022-10-30 18:50:51 -0300

    calculadora em progresso

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 class Calculator {
-   constructor(previousOperandTextElement, currentOperandTextElement)
-   this.previousOperandTextElement = previousOperandTextElement
-   this.currentOperandTextElement = currentOperandTextElement
+   constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement
+      this.currentOperandTextElement = currentOperandTextElement
+      this.clear()
+   }
+
+   clear() {
+      this.currentOperand = ''
+      this.previousOperand = ''
+      this.operation = undefined;
+   }
+
+   delete() {
+
+   }
+
+   appendNumber(number) {
+      this.currentOperand = number
+   }
+
+   chooseOperation(operation) {
+
+   }
+
+   compute() {
+
+   }
+
+   updateDisplay() {
+      this.currentOperandTextElement.innerText = this.currentOperand
+   }
 }
 
 const numberButtons = document.querySelector('[data-number]')
@@ -11,3 +39,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+   })
+})
